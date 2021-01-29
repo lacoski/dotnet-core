f628f51af75932d6c2781b6a4e8bb09ae047e646
Nguyen Ba Thanh <thanh@gmail.com>
2021-01-29 16:44:40 +0700

    update

---
+++ 3.Advance/MultiFileSample/Function.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace functionSample
+{
+    class FunctionUtils
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ 3.Advance/MultiFileSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using functionSample;
+
+namespace MultiFileSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            FunctionUtils func = new FunctionUtils();
+            int a = 4, b = 8;
+            int c = func.Add(a, b);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, c); 
+        }
+    }
+}
